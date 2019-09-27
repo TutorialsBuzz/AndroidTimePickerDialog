3179da251a97af73ee89f25eaba945557298a0f7
Pawan Deshpande <Pawan.Deshpande@ril.com>
2019-09-27 15:57:09 +0530

    first commit

---
+++ app/src/main/java/com/tutorialsbuzz/androidtimepicker/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.tutorialsbuzz.androidtimepicker
+
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.widget.TimePicker
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mTimePicker: TimePickerDialog
+        val mcurrentTime = Calendar.getInstance()
+        val hour = mcurrentTime.get(Calendar.HOUR_OF_DAY)
+        val minute = mcurrentTime.get(Calendar.MINUTE)
+
+        mTimePicker = TimePickerDialog(this, object : TimePickerDialog.OnTimeSetListener {
+            override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+                selectedTime.setText(String.format("%d : %d", hourOfDay, minute))
+            }
+        }, hour, minute, false)
+
+        selectTime.setOnClickListener({ v ->
+            mTimePicker.show()
+        })
+
+    }
+}
